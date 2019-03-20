e685d7ab9357374b5721ffee393b78e4106a1047
zhouwenpen <zhouwenpen@163.com>
2019-03-20 17:28:08 +0800

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -5,3 +5,13 @@
  * Date: 2019/3/20
  * Time: 16:37
  */
+
+function test_helper()
+{
+    return 'OK';
+}
+
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
