c51a710ae1f4af14418102875e55a2ccc5dad34e
zhouwenpen <zhouwenpen@163.com>
2019-03-22 11:15:30 +0800

    管理后台-新增、编辑商品

--- app/Models/ProductSku.php
+++ app/Models/ProductSku.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductSku extends Model
 {
-    //
+    protected $fillable = ['title', 'description', 'price', 'stock'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
