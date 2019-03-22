d1ccfc3252211730c50586966f736427635931c2
zhouwenpen <zhouwenpen@163.com>
2019-03-22 15:47:25 +0800

    异常处理

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\InvalidRequestException;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -50,7 +51,7 @@ class ProductsController extends Controller
     public function show(Request $request, Product $product)
     {
         if (!$product->on_sale) {
-            throw new \Exception('商品未上架');
+            throw new InvalidRequestException('商品未上架');
         }
         return view('products.show',['product' => $product]);
     }
